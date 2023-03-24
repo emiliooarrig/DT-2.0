93664aee87e88d0a40f652f1fe562d3d002a4c0b
Emilio Arriaga Guzman <111543168+emiliooarrig@users.noreply.github.com>
2023-03-24 03:39:32 +0000

    agregamos el codigo js para el carousel

--- galeria/transiciones.js
+++ galeria/transiciones.js
@@ -1,24 +1,28 @@
-let num1 = parseFloat(prompt("Ingrese el primer número: "));
-let num2 = parseFloat(prompt("Ingrese el segundo número: "));
-let operacion = prompt("Ingrese la operación a realizar (+, -, *, /): ");
+let slideIndex = 1;
+showSlides(slideIndex);
 
-let resultado;
-
-switch(operacion) {
-  case "+":
-    resultado = num1 + num2;
-    break;
-  case "-":
-    resultado = num1 - num2;
-    break;
-  case "*":
-    resultado = num1 * num2;
-    break;
-  case "/":
-    resultado = num1 / num2;
-    break;
-  default:
-    console.log("Operación inválida");
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-console.log(`El resultado de la operación es: ${resultado}`);
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
